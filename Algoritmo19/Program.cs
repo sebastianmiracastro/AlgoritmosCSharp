489ba75fe4e0902071e051cb7e7e6f1f782e3d9f
sebastianmiracastro <smira44@misena.edu.co>
2021-10-25 17:47:12 -0500

    Examen #2, Terminar Ejercicio 18

---
+++ Algoritmo19/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Algoritmo19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pongale Un Limite Al Contador: ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= Num; i++)
+            {
+                for(int x = 1; x <= i; x++)
+                {
+                    Console.Write(" " + x);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
